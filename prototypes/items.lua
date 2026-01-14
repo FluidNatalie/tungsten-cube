2c7407f8bd2f48fe3d9934fea553a12a49924b58
FluidNatalie <nateandgeorge@gmail.com>
2026-01-14 20:13:54 +0100

    You can now throw the Cube!

--- prototypes/items.lua
+++ prototypes/items.lua
@@ -1,12 +1,44 @@
 
     data:extend({
  	{
- 		type = "item",
+ 		type = "capsule",
  		name = "tungsten-cube",
 		weight = 1000*kg,
 		place_as_equipment_result = "tungsten-cube",
 		icon = "__tungsten-cube__/icons/tungsten_cube.png",
 		stack_size = 1,
- 		auto_recycle = true
+ 		auto_recycle = true,
+
+		capsule_action = {
+			type = "throw",
+			uses_stack = true, --add a setting to make the be toggled later
+			attack_parameters = {
+				type = "projectile",
+				sound = { filename = "__tungsten-cube__/sounds/throw.ogg" }, --find out if I can add an alternative setting to just be the audio of the video
+				range = 20,
+				cooldown = 30,
+				activation_type = "throw",
+				damage_modifier = 100000000,
+				ammo_category = "capsule",
+				ammo_type = {
+					action = {
+						type = "area",
+						radius = 3,
+						show_in_tooltip = false,
+						target_enemies = true,
+						action_delivery = { --https://lua-api.factorio.com/latest/types/TriggerDelivery.html --replace with projectile at some later point
+							type ="instant",
+							target_effects = {
+								type ="damage",
+								damage = {
+									amount = 1000000000,
+									type = "impact"
+								}
+							}
+						}
+					}
+				}
+			}
+		}
     }})
 
