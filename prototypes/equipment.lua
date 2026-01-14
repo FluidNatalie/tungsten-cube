55d87c3eb8ccb3bcbcba57ba17bb04074811673b
FluidNatalie <nateandgeorge@gmail.com>
2026-01-14 17:55:59 +0100

    Improved the Cube

---
+++ prototypes/equipment.lua
@@ -0,0 +1,19 @@
+
+    data:extend({
+ 	{
+ 		type = "inventory-bonus-equipment",
+ 		name = "tungsten-cube",
+		inventory_size_bonus = 20,
+		sprite = {
+			filename = "__tungsten-cube__/icons/tungsten_cube.png",
+			size = 64
+		},
+		shape = {
+			width = 1,
+			height = 1,
+			type = "full"
+		},
+		categories = {"armor"},
+		take_result = "tungsten-cube"
+    }})
+
--- prototypes/items.lua
+++ prototypes/items.lua
@@ -4,6 +4,7 @@
  		type = "item",
  		name = "tungsten-cube",
 		weight = 1000*kg,
+		place_as_equipment_result = "tungsten-cube",
 		icon = "__tungsten-cube__/icons/tungsten_cube.png",
 		stack_size = 1,
  		auto_recycle = true
