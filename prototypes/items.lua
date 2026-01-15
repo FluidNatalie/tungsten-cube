9f0a6ad0408f754c4dd07ec1d287a3890607bf84
FluidNatalie <nateandgeorge@gmail.com>
2026-01-15 14:31:11 +0100

    4.0.0 - Added more cubes

--- prototypes/items.lua
+++ prototypes/items.lua
@@ -1,8 +1,20 @@
-
-    data:extend({
- 	{
+	data:extend({
+		{
+			type = "item-group",
+			name = "cubes-group",
+			icon = "__tungsten-cube__/icons/cubes_group.png",
+			icon_size = 128,
+			order = "e1[cubes]"
+		},
+		{
+			type = "item-subgroup",
+			name = "cubes-subgroup",
+			group = "cubes-group"
+		},
+		{
  		type = "capsule",
  		name = "tungsten-cube",
+		subgroup = "cubes-subgroup",
 		weight = 1000*kg,
 		place_as_equipment_result = "tungsten-cube",
 		icon = "__tungsten-cube__/icons/tungsten_cube.png",
@@ -40,5 +52,61 @@
 				}
 			}
 		}
-    }})
+    },
+	{
+		type = "item", --turn this into artillery ammo
+ 		name = "iron-cube",
+		subgroup = "cubes-subgroup",
+		weight = 1000*kg,
+		icon = "__tungsten-cube__/icons/iron_cube.png",
+		stack_size = 1,
+ 		auto_recycle = true
+	},
+	{
+		type = "item",
+ 		name = "copper-cube",
+		subgroup = "cubes-subgroup",
+		weight = 1000*kg,
+		icon = "__tungsten-cube__/icons/copper_cube.png",
+		stack_size = 1,
+ 		auto_recycle = true
+	},
+	{
+		type = "item", --turn this into an insanely strong fuel
+ 		name = "carbon-cube",
+		subgroup = "cubes-subgroup",
+		weight = 500*kg,
+		icon = "__tungsten-cube__/icons/carbon_cube.png",
+		stack_size = 1,
+ 		auto_recycle = true
+	},
+	{
+		type = "item",
+ 		name = "holmium-cube",
+		subgroup = "cubes-subgroup",
+		weight = 1000*kg,
+		icon = "__tungsten-cube__/icons/holmium_cube.png",
+		stack_size = 1,
+ 		auto_recycle = true
+	},
+	{
+		type = "item",
+ 		name = "biter-egg-cube",
+		subgroup = "cubes-subgroup",
+		weight = 1000*kg,
+		icon = "__tungsten-cube__/icons/biter_egg_cube.png",
+		icon_size = 128,
+		stack_size = 1,
+ 		auto_recycle = true
+	},
+	{
+		type = "item", --make this throwable and a nuke
+ 		name = "u-235-cube",
+		subgroup = "cubes-subgroup",
+		weight = 1000*kg,
+		icon = "__tungsten-cube__/icons/uranium235_cube.png",
+		stack_size = 1,
+ 		auto_recycle = true
+	}
+})
 
--- prototypes/recipe.lua
+++ prototypes/recipe.lua
@@ -3,6 +3,7 @@
  	{
  		type = "recipe",
  		name = "tungsten-cube-recipe",
+		subgroup = "cubes-subgroup",
  		category = "crafting",
  		energy_required = 3600,
  		ingredients = {
@@ -14,5 +15,102 @@
  		hide_from_signal_gui = false,
  		auto_recycle = true,
         enabled = false
-    }})
+    },
+	{
+ 		type = "recipe",
+ 		name = "biter-egg-cube-recipe",
+		subgroup = "cubes-subgroup",
+ 		category = "crafting",
+ 		energy_required = 300,
+ 		ingredients = {
+ 			{ type = "item", name = "biter-egg", amount = 500 },
+ 		},
+ 		results = {
+ 			{ type = "item", name = "biter-egg-cube", amount = 1 },
+ 		},
+ 		hide_from_signal_gui = false,
+ 		auto_recycle = true,
+        enabled = true --set to false once a tech has been added
+    },
+	{
+ 		type = "recipe",
+ 		name = "carbon-cube-recipe",
+		subgroup = "cubes-subgroup",
+ 		category = "crafting",
+ 		energy_required = 300,
+ 		ingredients = {
+ 			{ type = "item", name = "carbon", amount = 500 },
+ 		},
+ 		results = {
+ 			{ type = "item", name = "carbon-cube", amount = 1 },
+ 		},
+ 		hide_from_signal_gui = false,
+ 		auto_recycle = true,
+        enabled = true --set to false once a tech has been added
+    },
+	{
+ 		type = "recipe",
+ 		name = "copper-cube-recipe",
+		subgroup = "cubes-subgroup",
+ 		category = "crafting",
+ 		energy_required = 600,
+ 		ingredients = {
+ 			{ type = "item", name = "copper-plate", amount = 1000 },
+ 		},
+ 		results = {
+ 			{ type = "item", name = "copper-cube", amount = 1 },
+ 		},
+ 		hide_from_signal_gui = false,
+ 		auto_recycle = true,
+        enabled = true --set to false once a tech has been added
+    },
+	{
+ 		type = "recipe",
+ 		name = "holmium-cube-recipe",
+		subgroup = "cubes-subgroup",
+ 		category = "crafting",
+ 		energy_required = 1800,
+ 		ingredients = {
+ 			{ type = "item", name = "holmium-plate", amount = 500 },
+ 		},
+ 		results = {
+ 			{ type = "item", name = "holmium-cube", amount = 1 },
+ 		},
+ 		hide_from_signal_gui = false,
+ 		auto_recycle = true,
+        enabled = true --set to false once a tech has been added
+    },
+	{
+ 		type = "recipe",
+ 		name = "iron-cube-recipe",
+		subgroup = "cubes-subgroup",
+ 		category = "crafting",
+ 		energy_required = 600,
+ 		ingredients = {
+ 			{ type = "item", name = "iron-plate", amount = 1000 },
+ 		},
+ 		results = {
+ 			{ type = "item", name = "iron-cube", amount = 1 },
+ 		},
+ 		hide_from_signal_gui = false,
+ 		auto_recycle = true,
+        enabled = true --set to false once a tech has been added
+    },
+	{
+ 		type = "recipe",
+ 		name = "u-235-cube-recipe",
+		subgroup = "cubes-subgroup",
+ 		category = "crafting",
+ 		energy_required = 600,
+ 		ingredients = {
+ 			{ type = "item", name = "uranium-ore", amount = 500 }, --recipe is subject to change
+ 		},
+ 		results = {
+ 			{ type = "item", name = "u-235-cube", amount = 1 },
+ 		},
+ 		hide_from_signal_gui = false,
+ 		auto_recycle = true,
+        enabled = true --set to false once a tech has been added
+    },
+})
 
